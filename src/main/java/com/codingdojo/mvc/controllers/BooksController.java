3a6bbbc2eca05936ee5d642fb2736765725f53c7
Maria Stezhko <stezhko@gmail.com>
2018-06-14 11:24:48 -0700

    Add Show method

--- src/main/java/com/codingdojo/mvc/controllers/BooksController.java
+++ src/main/java/com/codingdojo/mvc/controllers/BooksController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -41,6 +42,12 @@ private final BookService bookService;
             bookService.createBook(book);
             return "redirect:/books";
         }
+    }
+    @RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.findBook(id);
+        model.addAttribute("book", book);
+        return "/books/show.jsp";
     }
 
 }
